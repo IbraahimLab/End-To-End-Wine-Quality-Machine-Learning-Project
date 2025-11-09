d623e59b4281a5dde82c3f53ec5dd8b41024728e
ibraahim <ibraakadarba.12@gmail.com>
2025-11-09 14:57:08 +0300

    constant

---
+++ setup.py
@@ -0,0 +1,10 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name="Wine_Quality",
+    version="0.0.0",
+    author="Ibraahim Ahmed",
+    author_email="ibraakadarba.12gmail.com",
+    packages=find_packages()
+)
