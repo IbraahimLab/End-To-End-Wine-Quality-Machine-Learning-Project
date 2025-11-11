085ed25a64a483361d1c91b6e57fc0f29990c134
ibraahim <ibraakadarba.12@gmail.com>
2025-11-11 09:02:30 +0300

    ci cd

--- test.py
+++ test.py
@@ -4,3 +4,7 @@ from wine_quality.constants import *
 
 response = s3.list_objects_v2(Bucket='model-registry')
 print(response)
+
+
+# The response will contain a list of objects in the specified S3 bucket.
+
